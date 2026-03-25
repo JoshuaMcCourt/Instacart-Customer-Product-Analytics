24f2505ee69f199b1655ec71865b4f2d5d6e107d
JoshuaMcCourt <joshuamccourt@hotmail.co.uk>
2026-03-25 16:05:46 +0000

    Update full_script.sql

--- full_script.sql
+++ full_script.sql
@@ -1191,6 +1191,36 @@ GO
 -- 06_export_files.sql
 -- Export processed marts tables to CSV 
 
+USE [InstacartAnalyticsWarehouse];
+GO
+
+-- Reduce fact_orders for Tableau usage
+-- Drop table if exists
+
+IF OBJECT_ID('dbo.fact_orders_tableau', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_orders_tableau;
+GO
+
+-- Table Creation
+SELECT *
+INTO dbo.fact_orders_tableau
+FROM dbo.fact_orders
+WHERE user_id % 10 = 0
+  AND order_number <= 20;
+
+-- Reduce fact_user_product for Tableau usage
+-- Drop table if exists
+
+IF OBJECT_ID('dbo.fact_user_product_tableau', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_user_product_tableau;
+GO
+
+-- Table Creation
+SELECT *
+INTO dbo.fact_user_product_tableau
+FROM dbo.fact_user_product
+WHERE user_id % 10 = 0
+
 -- 1) dim_users
 SELECT * 
 FROM dbo.dim_users;
@@ -1203,11 +1233,19 @@ FROM dbo.dim_products;
 SELECT * 
 FROM dbo.fact_orders;
 
--- 4) fact_user_product
+-- 4) fact_orders_tableau
+SELECT * 
+FROM dbo.fact_orders_tableau;
+
+-- 5) fact_user_product
 SELECT * 
 FROM dbo.fact_user_product;
 
--- 5) fact_user_cohorts
+-- 6) fact_user_product_tableau
+SELECT * 
+FROM dbo.fact_user_product_tableau;
+
+-- 7) fact_user_cohorts
 SELECT * 
 FROM dbo.fact_user_cohorts;
 
